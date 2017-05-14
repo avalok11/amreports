bb406e69d55dce832e9a7c4314c9dc63c6686370
avalok11 <Djljrfyfkvjq1>
2017-05-14 17:04:06 +0300

    get the list of 0 NDS checks

--- OFD/ofd_tests.py
+++ OFD/ofd_tests.py
@@ -4,7 +4,7 @@
 import requests
 import json
 import pandas as pd
-import csv
+import datetime
 
 
 def connect(idd='1025293145607151', login='yarkov.aleksei', pwd='331812qweASD'):
@@ -62,16 +62,67 @@ def list_fn(cooks, regid, inn='7825335145', status=2):
     :return:
     """
     response = requests.get('https://api.sbis.ru/ofd/v1/orgs/'+str(inn)+'/kkts/'+str(regid)+'/storages?status='
-                            +str(status),cookies=cooks)
+                            + str(status), cookies=cooks)
     return response.json()
 
 
+def list_checks(cooks, regid, storageid, datefrom, inn='7825335145'):
+    response = requests.get('https://api.sbis.ru/ofd/v1/orgs/'+str(inn)+'/kkts/'+str(regid)+'/storages/'
+                            + str(storageid)+'/docs?dateFrom='+str(datefrom)+'&limit=1000', cookies=cooks)
+    return response
+
+
+def search_nds0(doc):
+    check = pd.DataFrame()
+    j = 0
+    for d in doc:
+        nds0 = None
+        ndsCalculated10 = None
+        ndsCalculated18 = None
+        fiscalDriveNumber = None
+        fiscalDocumentNumber = None
+        shiftNumber = None
+        dateTime = None
+        print '\nНайденный чек:'
+        if 'receipt' in d:
+            print j
+            j += 1
+            print d
+            if 'nds0' in d['receipt']:
+                nds0 = d['receipt']['nds0']
+            if 'ndsCalculated10' in d['receipt']:
+                ndsCalculated10 = d['receipt']['ndsCalculated10']
+            if 'ndsCalculated18' in d['receipt']:
+                ndsCalculated18 = d['receipt']['ndsCalculated18']
+            if 'fiscalDocumentNumber' in d['receipt']:
+                fiscalDocumentNumber = d['receipt']['fiscalDocumentNumber']
+            if 'dateTime' in d['receipt']:
+                dateTime = datetime.datetime.fromtimestamp(int(d['receipt']['dateTime'])).strftime('%Y-%m-%d %H:%M:%S')
+            if 'shiftNumber' in d['receipt']:
+                shiftNumber = d['receipt']['shiftNumber']
+            if 'fiscalDriveNumber' in d['receipt']:
+                fiscalDriveNumber = d['receipt']['fiscalDriveNumber']
+        if nds0 is not None:
+            detail = pd.Series([fiscalDriveNumber, fiscalDocumentNumber, shiftNumber, dateTime, nds0,
+                                ndsCalculated10, ndsCalculated18], index=['fiscalDriveNumber', 'fiscalDocumentNumber',
+                                                                            'shiftNumber', 'dateTime', 'ndsNo',
+                                                                            'ndsCalculated10', 'ndsCalculated18'])
+            check = check.append(detail, ignore_index=True)
+    return check
+
+
 def main():
+    # ====================
+    # АТОРИЗАЦИЯ
+    # ====================
     connection, cooks = connect()
     print (connection.status_code)
     print (connection.url)
     print (cooks)
 
+    # ====================
+    # ПОЛУЧЕНИЕ СПИСКА ПРИНТЕРОВ
+    # ====================
     kkts = list_kkt(cooks)
     print "Список ККТ по организации"
     # print kkts
@@ -81,58 +132,64 @@ def main():
     kkt_list.drop('status', axis=1, inplace=True)
     kkt_list.to_csv('df.csv', sep=';', encoding='utf-8')
 
+    # ====================
+    # ДЛЯ ТЕСТА ОГРАНИЧИВАЕМ КОЛ-ВО ПРИНТЕРОВ
+    # ====================
+    #kkt_list = kkt_list.iloc[0:10]
+    #print kkt_list
+
+
+    # ====================
+    # ПОЛУЧЕНИЕ СПИСКА ФН ПО КАЖДОМУ ПРИНТЕРУ
+    # ====================
     fn_list = pd.DataFrame()
     for regId in kkt_list.index.values:
-        #print pd.DataFrame(list_fn(cooks, regId, inn='7825335145', status=2))
+        print "Фискальный накопитель принтера номер: ", regId, ". По адресу: ", kkt_list.loc[regId]['address']
         dat = pd.DataFrame(list_fn(cooks, regId, inn='7825335145', status=2))
         dat['regId'] = regId
         fn_list = pd.concat([fn_list, dat])
-        print fn_list
+        print dat
     fn_list.to_csv('fn_list.csv', sep=';', encoding='utf-8')
 
+    # ====================
+    # ОБЬЕДИНЕНИЕ ФН И ПРИНТЕРОВ В ОДИН СПИСОК
+    # ====================
     kkt_list.reset_index(inplace=True)
     full_data = pd.merge(kkt_list, fn_list, how='inner', on='regId')
     full_data.to_csv('full_list.csv', sep=';', encoding='utf-8')
 
+    # ====================
+    # Список всех фискальных документов по всем фискальном накопителям в организации
+    # ====================
+    all_docs = pd.DataFrame()
+    print "список фискальных документов"
+
+    for i in range(len(full_data)):
+        print "Порядковый номер принтера: ", i
+        regid = full_data.iloc[i]['regId']
+        storageid = full_data.iloc[i]['storageId']
+        print regid, storageid
+        #regid = '0000182040024937'
+        #storageid = '8710000100086130'
+        r = list_checks(cooks, regid, storageid, '2017-05-13T00:00:00')
+        r = r.json()
+        doc = search_nds0(r)
+        all_docs = pd.concat([all_docs, doc])
+
+#    print all_docs
+    all_docs.to_csv('list_ndsNo.csv', sep=';', encoding='utf-8')
+
+    full_data_with_checks = pd.merge(full_data, all_docs, how='inner', left_on='storageId', right_on='fiscalDriveNumber')
+    full_data_with_checks.to_csv('list_ndsNo_addr.csv', sep=';', encoding='utf-8')
+
     connection.close()
-
-
-
-    # print kkt_list[['regId', 'factoryId', 'address']]
-
-
-
-
-
-    #i = 0
-    #for a in kkts:
-    #print "ID:", kkts['regId'], "Factor", kkts['factoryId'], "Model: ", kkts['model'], "Адрес: ", kkts['address']
-    #    i += 1
-    #print i
-
-    #print "\nFN=++++++++++++"
-    #df_kkts = pd.DataFrame(kkts)
-    #df_kkts.set_index('address', inplace=True)
-    #df_kkts.sort_index(inplace=True)
-    #print df_kkts
-
-
-
-
-
-    #print "Список фискальных документов по фискальному накопителю"
-    #r = requests.get('https://api.sbis.ru/ofd/v1/orgs/7825335145/kkts/0000292532034264/storages/8710000100082845/'
-    #                 'docs?dateFrom=2017-05-01T12:12:12&limit=5', cookies=cooks)
-    #print "Получение фискального документа по идентификатору"
-    #r = requests.get('https://api.sbis.ru/ofd/v1/orgs/7825335145/kkts/0000292532034264/storages/8710000100082845/'
-    #                 'docs/77?format=json', cookies=cooks)
-
-
-    # print('JSON')
-    # r = requests.get('https://api.sbis.ru/v1/orgs/7825335145/kkts?status=2', params=js)
-    # print (r.status_code)
-    # print (r.url)
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+        #requests.get('https://api.sbis.ru/ofd/v1/orgs/7825335145/kkts/0000182040024937/storages/8710000100086130/'
+                     #'docs?dateFrom=2017-05-01T00:00:00&limit=5', cookies=cooks)
